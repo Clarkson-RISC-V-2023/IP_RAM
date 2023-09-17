025fe2d1a8897c2f66fdda9693dde67b905e7a23
Ernesto Sola-Thomas <ernesto@solathomas.com>
2023-09-17 16:49:06 -0400

    Fix rom rtl and tb

--- rtl/rom.sv
+++ rtl/rom.sv
@@ -1,22 +1,23 @@
-module rom (
-    parameter INSTR_WIDTH 32,
-    parameter INSTR_COUNT 256,
-    parameter DEPTH INSTR_COUNT*INSTR_WIDTH/4        
-    )(
-        input wire clk,
-        input wire [$clog2(DEPTH)-1:0] addr_i,
-        output [INSTR_WIDTH-1:0] rom_instr
-    );
+`timescale 1ns/1ps
 
-    memb #(
+module rom #(
+    parameter DEPTH = 256, 
+    parameter DATA_WIDTH = 32,
+    parameter string MEM_INIT_PATH = ""
+    )(
+    input wire clk,
+    input wire [$clog2(DEPTH)-1:0] addr_i,
+    output logic [DATA_WIDTH-1:0] rom_o
+);
+    memblock #(
         .DEPTH(DEPTH),
-        .DATA_WIDTH(8)
-    ) memblock (
+        .DATA_WIDTH(DATA_WIDTH),
+        .MEM_INIT_PATH(MEM_INIT_PATH)
+    ) mem_inst (
         .clk(clk),
-        .wr_en_i(1'b0), // ROM read only
-        .addr_i(addr_i),
-        .wr_data_i(8'b00000000);
-        .rd_data_o(rom_instr)
+        .wr_en_i(1'b0), // Write enable is always low for ROM
+        .addr_i(addr_i >> 2), // Right shift address by 2 to increment by 4
+        .wr_data_i('0), // Write data is don't care for ROM
+        .rd_data_o(rom_o)
     );
-    
-endmodule;
+endmodule
--- verif/tb_rom.sv
+++ verif/tb_rom.sv
@@ -1,45 +1,59 @@
 `timescale 1ns/1ps
-`define MEM_INIT_PATH ""
+`define ROM_INIT_PATH ""
 
 module tb_rom #(
-    parameter INSTR_WIDTH 32,
-    parameter INSTR_COUNT 10,
-    parameter DEPTH INSTR_COUNT*INSTR_WIDTH/4
+    parameter INSTR_WIDTH = 32,
+    parameter INSTR_COUNT = 10,
+    parameter string MEM_INIT_PATH = `ROM_INIT_PATH
 )(
     // EMPTY 
 );
 
+    localparam DEPTH = INSTR_COUNT*INSTR_WIDTH/4;
+
     reg clk;
     reg [$clog2(DEPTH)-1:0] addr;
-    wire instr;
+    wire [INSTR_WIDTH-1:0] instr;
 
-    dut_rom #(
-        .INSTR_WIDTH(INSTR_WIDTH),
-        .INSTR_COUNT(INSTR_COUNT),
+    rom #(
+        .DATA_WIDTH(INSTR_WIDTH),
         .DEPTH(DEPTH)
-    ) rom (
+    ) dut_rom (
         .clk(clk),
         .addr_i(addr),
-        .rom_instr(instr)
+        .rom_o(instr)
     );
 
     always #10 clk= ~clk;
 
+
+
     initial begin
-        $readmemh(`MEM_INIT_PATH, dut_rom.memb.bmem);
+        $readmemh(MEM_INIT_PATH, dut_rom.mem_inst.bmem);
         $dumpfile("rom_tb.vcd");
         $dumpvars(0, tb_rom);
+        $display("ROM data loaded from %s", MEM_INIT_PATH);
         clk = 1'b0;
         addr = {$clog2(DEPTH){1'b0}};
-        #20 addr + 1'b1;
-        #20 addr + 1'b1;
-        #20 addr + 1'b1;
-        #20 addr + 1'b1;
-        #20 addr + 1'b1;
-        #20 addr + 1'b1;
-        #20 addr + 1'b1;
-        #20 addr + 1'b1;
-        #20 addr + 1'b1;
+        #20 addr = addr + 3'b100;
+        #20 addr = addr + 3'b100;
+        #20 addr = addr + 3'b100;
+        #20 addr = addr + 3'b100;
+        #20 addr = addr + 3'b100;
+        #20 addr = addr + 3'b100;
+        #20 addr = addr + 3'b100;
+        #20 addr = addr + 3'b100;
+        #20 addr = addr + 3'b100;
+        // Now go down
+        #20 addr = addr - 3'b100;
+        #20 addr = addr - 3'b100;
+        #20 addr = addr - 3'b100;
+        #20 addr = addr - 3'b100;
+        #20 addr = addr - 3'b100;
+        #20 addr = addr - 3'b100;
+        #20 addr = addr - 3'b100;
+        #20 addr = addr - 3'b100;
+        #20 addr = addr - 3'b100;
         #50 $finish;
     end
 endmodule
