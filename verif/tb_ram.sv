d3e36937137a94db2d3a2eb7d439a6835ee6eb99
Ernesto Sola-Thomas <ernesto@solathomas.com>
2023-09-18 22:18:04 -0400

    Extend tb with starting write and read from address offset 1

--- verif/tb_ram.sv
+++ verif/tb_ram.sv
@@ -45,6 +45,7 @@ module tb_ram;
         mem_block_en_i = 4'b1111; // Enable all memory blocks
 
         // Write to the RAM
+
         for (addr_i = 0; addr_i < ADDRESS_SPACE; addr_i = addr_i + 4) begin
             wr_data_i = addr_i;
             wr_en_i = 1;
@@ -58,11 +59,36 @@ module tb_ram;
 
         // Read from the RAM and check the data
         for (addr_i = 0; addr_i < ADDRESS_SPACE; addr_i = addr_i + 4) begin
-            #20; // Wait for two clock cycle
+            #10; // Wait for a clock cycle
             if (rd_data_o !== addr_i) begin
                 $display("Data mismatch at address %d: expected %d, got %d", addr_i, addr_i, rd_data_o);
             end
             if (addr_i == ADDRESS_SPACE-4) begin
+                $display("Test completed");
+                break;
+            end 
+        end
+
+        // Write to the RAM with offset
+
+        for (addr_i = 1; addr_i < ADDRESS_SPACE; addr_i = addr_i + 4) begin
+            wr_data_i = addr_i;
+            wr_en_i = 1;
+            #10; // Wait for a clock cycle
+            wr_en_i = 0;
+            #10; // Wait for a clock cycle
+            if (addr_i == ADDRESS_SPACE-7) begin
+                break;
+            end
+        end
+
+        // Read from the RAM and check the data
+        for (addr_i = 1; addr_i < ADDRESS_SPACE; addr_i = addr_i + 4) begin
+            #10; // Wait for a clock cycle
+            if (rd_data_o !== addr_i) begin
+                $display("Data mismatch at address %d: expected %d, got %d", addr_i, addr_i, rd_data_o);
+            end
+            if (addr_i == ADDRESS_SPACE-7) begin
                 $display("Test completed");
                 $finish;
             end 
