b9bb5e2691b9def84b2404794249df94017bf659
Ernesto Sola-Thomas <ernesto@solathomas.com>
2023-09-29 21:38:55 -0400

    stash changes

--- verif/uvm_ram.sv
+++ verif/uvm_ram.sv
@@ -11,6 +11,42 @@
 // Include UVM 
 `include "uvm_macros.svh"
 
+class ram_test extends uvm_test;
+    `uvm_component_utils(ram_test)
+
+    ram_env r_env;
+
+    function new(string name = "RAM_test", uvm_component parent = null);
+        super.new(name, parent);
+    endfunction //new()
+
+    virtual function void build_phase(uvm_phase phase);
+        super.build_phase(phase);
+        r_env = my_env::type_id::create("r_env", this);
+    endfunction //build_phase()
+endclass : ram_test
+
+class ram_env extends uvm_env;
+    `uvm_component_utils(ram_env);
+
+    ram_agent r_agent;
+    ram_scoreboard r_scoreboard;
+
+    function new(string name = "ram_env", uvm_component parent = null);
+        super.new(name, parent);
+    endfunction : new
+
+    virtual function void build_phase(uvm_phase phase);
+        super.build_phase(phase);
+        r_agent = ram_agent::type_id::create("r_agent", this);
+        r_scoreboard = ram_scoreboard::type_id::create("r_scoreboard", this);
+    endfunction : build_phase
+
+    virtual function void connect_phase(uvm_phase phase);
+        super.connect_phase(phase);
+        r_agent.ap.connect(r_scoreboard.analysis_export);
+    endfunction : connect_phase
+endclass : ram_env
 
 // Define Agent class
 
@@ -29,7 +65,7 @@ class ram_sequence_item extends uvm_sequence_item;
     // RAM transaction package 
     bit [31:0] addr;    // Address for the package
     bit [31:0] data;    // Data to write or read
-    bit wr_en;          // Write enable bit
+    bit write_enable;          // Write enable bit
 
     // TODO this should only be constraint for Full Word
     constraint addr_constraint {
@@ -48,7 +84,7 @@ class ram_sequence_item extends uvm_sequence_item;
         printer.field("Packet Information: ");
         printer.field_int("addr", addr, 32, UVM_HEX);
         printer.field_int("data", data, 32, UVM_HEX);
-        printer.field_int("wr_en", wr_en, 1, UVM_HEX);
+        printer.field_int("write_enable", write_enable, 1, UVM_HEX);
     endfunction : do_print
 
     function bit do_compare (uvm_object obj_to_compare);//, uvm_comparer comparer);
@@ -56,7 +92,7 @@ class ram_sequence_item extends uvm_sequence_item;
         if (!$cast(ram_item_compare, obj_to_compare)) // cast copies over obj_to_compare to ram_item_compare
             return 0;
 
-        if (addr !== ram_item_compare.addr || data !== ram_item_compare.data || write !== ram_item_compare.write)
+        if (addr !== ram_item_compare.addr || data !== ram_item_compare.data || write_enable !== ram_item_compare.write_enable)
             return 0;
 
         return 1;
@@ -85,9 +121,9 @@ class ram_sequence_rw_type extends uvm_sequence#(ram_sequence_item);
         ram_seq_item.randomize(); // Randomize Packet data and address
 
         if (randomize::random() % 2 == 0)
-            ram_seq_item.wr_en = 1; // Write Operation
+            ram_seq_item.write_enable = 1; // Write Operation
         else
-            ram_seq_item.wr_en = 0; // Read Operation
+            ram_seq_item.write_enable = 0; // Read Operation
 
         ram_seq_item.star();
 
@@ -114,3 +150,6 @@ endclass : ram_sequencer
 
 // Define Monitor
 
+// module top;
+//   initial run_test("my_test");
+// endmodule : top
