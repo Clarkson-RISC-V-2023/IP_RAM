cc7f6f53fccf55affd5e6ed407711c04c71432da
Ernesto Sola-Thomas <ernesto@solathomas.com>
2023-10-03 15:30:13 -0400

    Initial state of RAM sequencer item

---
+++ verif/uvm/ram_packet_item.svh
@@ -0,0 +1,34 @@
+// Base UVM transaction item
+
+`include "uvm_macros.svh"
+import uvm_pkg::*;
+
+import ram_params::*;
+
+class ram_packet_items extends uvm_sequence_item;
+    rand bit [$clog2(ADDRESS_SPACE)-1:0]    addr;
+    rand bit [DATA_WIDTH-1:0]               wdata;
+    rand bit [NUM_OF_MEM_BLOCKS-1:0]        mem_block_en;
+    rand bit                                wr_en_i;
+    bit      [DATA_WIDTH-1:0]               rdata;
+
+    constraint ram_item_addr { addr inside {[0:DEPTH]}; };
+
+    // Use macro to register class with factory
+    `uvm_object_utils_begin(ram_packet_items)
+        `uvm_field_int(addr, UVM_DEFAULT)
+        `uvm_field_int(wdata, UVM_DEFAULT)
+        `uvm_field_int(rdata, UVM_DEFAULT)
+        `uvm_field_int(mem_block_en, UVM_DEFAULT)
+        `uvm_field_int(wr_en_i, UVM_DEFAULT)
+    `uvm_object_utils_end
+
+    function new (string name = "DEFAULT ram_packet_item");
+        super.new(name);
+    endfunction
+
+    virtual function string convert2string();
+        return $sformatf("addr=0x%0h - wdata=0x%0h - rdata=0x%0h - wr=%0d - mem_block_en=0x%h", addr, wdata, rdata, wr_en_i, mem_block_en);
+    endfunction
+
+endclass
