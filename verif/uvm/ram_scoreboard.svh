87565106ac21a513ce9cc63f99e20ef18bee40e9
Ernesto Sola-Thomas <ernesto@solathomas.com>
2023-10-04 18:13:45 -0400

    Fix typo in scoreboard

--- verif/uvm/ram_scoreboard.svh
+++ verif/uvm/ram_scoreboard.svh
@@ -6,7 +6,7 @@ import ram_params::*;
 class ram_scoreboard extends uvm_scoreboard;
     
     `uvm_component_utils(ram_scoreboard)
-    function new(string name = "DEFAULT RAM scoreboard", uvm_component parent=null)'
+    function new(string name = "DEFAULT RAM scoreboard", uvm_component parent=null);
         super.new(name, parent);
     endfunction
 
@@ -27,17 +27,17 @@ class ram_scoreboard extends uvm_scoreboard;
                 `uvm_info(get_type_name(), $sformatf("New value written. %d of %d left to be written", mem_space, DEPTH), UVM_LOW);
             end
             refq[item.addr] = item;
-            `uvm_info(get_type_name(), $sformatf("Wrote 0x%0h to addr 0x%0h", itme.wdata, item.addr), UVM_LOW);
+            `uvm_info(get_type_name(), $sformatf("Wrote 0x%0h to addr 0x%0h", item.wdata, item.addr), UVM_LOW);
         end
 
         if(!item.wr_en) begin
             if(refq[item.addr] == null)
-                `uvm_error (get_type_name(), $sformatf("First time read addr=0x%0h. RAM initialization error, this address should have been written by sequence", item.addr), UVM_LOW)
+                `uvm_error (get_type_name(), $sformatf("First time read addr=0x%0h. RAM initialization error, this address should have been written by sequence", item.addr))
             else
                 if (item.rdata != refq[item.addr].wdata)
-                    `uvm_error(get_type_name(), $sformatf("Data missmatch, read addr 0x%0h and got 0x%h but 0x%h was expectedd...", item.addr, item.rdata, refq[item.addr].wdata), UVM_LOW)
+                    `uvm_error(get_type_name(), $sformatf("Data missmatch, read addr 0x%0h and got 0x%h but 0x%h was expectedd...", item.addr, item.rdata, refq[item.addr].wdata))
                 else 
-                    `uvm_info(get_type_name(), $sformatf("PASS! read addr 0x%0h got 0x%0h and mattched the expected 0x%h0", item.addr, item.rdata, refq[item.addr].wdata))
+                    `uvm_info(get_type_name(), $sformatf("PASS! read addr 0x%0h got 0x%0h and mattched the expected 0x%h0", item.addr, item.rdata, refq[item.addr].wdata), UVM_LOW)
         end
     endfunction
 endclass
