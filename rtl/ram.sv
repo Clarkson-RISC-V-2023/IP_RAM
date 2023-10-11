86ec11404deb0d3da5d90b531a1716ddca06a0c7
Ernesto Sola-Thomas <ernesto@solathomas.com>
2023-10-11 19:16:16 -0400

    Fix issue with generate block

--- rtl/ram.sv
+++ rtl/ram.sv
@@ -13,18 +13,18 @@ module ram (
 
     reg [$clog2(ADDRESS_SPACE)-1:0] bank_addr [NUM_OF_MEM_BLOCKS-1:0];
 
-    genvar bank_num;
     generate
+        genvar bank_num;  
         for (bank_num = 0; bank_num < NUM_OF_MEM_BLOCKS;bank_num=bank_num+1) begin : mem_block_loop
             localparam [$clog2(NUM_OF_MEM_BLOCKS)-1:0] bank_num_bin = bank_num;
-            assign bank_addr[bank_num] =  addr_i;
+            assign bank_addr[bank_num] = addr_i+bank_num_bin;
             memblock #(
                 .DEPTH(DEPTH),
                 .DATA_WIDTH(BANK_WIDTH)
             ) mem_bank (
                 .clk(clk),
                 .wr_en_i(wr_en_i & mem_block_en_i[bank_num]),
-                .addr_i((addr_i+bank_num_bin)>>2),
+                .addr_i(bank_addr[bank_num][$clog2(ADDRESS_SPACE)-1:$clog2(NUM_OF_MEM_BLOCKS)]),
                 .wdata_i(wdata_i[bank_num*BANK_WIDTH+:BANK_WIDTH]),
                 .rdata_o(rdata_o[bank_num*BANK_WIDTH+:BANK_WIDTH])
             );
